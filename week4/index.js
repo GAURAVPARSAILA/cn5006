c4d829b1dd3a105c028eda647b40a7d5213713f4
GAURAVPARSAILA <parsailagaurav2@gmail.com>
2025-10-22 10:56:38 +0000

    bb

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,42 @@
 
+var express=require("express")
+var fs= require("fs")
+var app=express()
+// add middle ware function for body parsing
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/',function(req,res){
+res.send("hello it is my first express application")
+})
+
+
+
+
+app.get('/about', function(req, res) {
+  res.send("This is basic express application");
+});
+
+app.get('/users/:userId/books/:bookId', function(req, res) {
+  res.send(req.params);
+});
+
+
+
+
+
+
+
+
+// app.get('/', function(req, res) {
+//   res.send("Welcome to the home page!");
+// });
+
+// app.get('/contact', function(req, res) {
+//   res.send("Contact us at contact@example.com");
+// });
+
+
+
+app.listen(5000,function(){console.log("server is running on port 5000")})
+
+
