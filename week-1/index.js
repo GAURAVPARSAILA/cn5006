582852c84b47c59c366be8c24b04f9f3ae3aac97
GAURAVPARSAILA <parsailagaurav2@gmail.com>
2025-10-22 10:55:51 +0000

    week 4

--- week-1/index.js
+++ week-1/index.js
@@ -1,1 +1,10 @@
+console.log('its mine new programe ')
+console.log('Well come naqi your month income is 700000'
 
+)
+const num1=5
+const num2=3
+//add two numbers
+const sum=num1+num2;
+//display the sum
+console.log('    the sum of'+num1+'and '+num2+' is:'+sum);
